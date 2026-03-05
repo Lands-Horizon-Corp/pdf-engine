359f6a93d2a48f001572da13e17947297bd7c870
zalven-official <zalvendayao888@gmail.com>
2026-03-05 11:26:11 +0800

    initial commit

--- src/services/user.rs
+++ src/services/user.rs
@@ -2,18 +2,16 @@ use crate::models::user::{CreateUserPayload, User};
 use crate::state::SharedState;
 
 pub fn create_user(state: &SharedState, payload: CreateUserPayload) -> User {
-    let mut users = state.users.write().unwrap(); // Get write lock
-
+    let mut users = state.users.write().unwrap();
     let new_user = User {
         id: (users.len() as u64) + 1,
         username: payload.username,
     };
-
     users.push(new_user.clone());
     new_user
 }
 
 pub fn get_all_users(state: &SharedState) -> Vec<User> {
-    let users = state.users.read().unwrap(); // Get read lock
+    let users = state.users.read().unwrap();
     users.clone()
 }

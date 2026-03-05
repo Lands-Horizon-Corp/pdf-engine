f088bac649115c07ea17cd904ac5f682ddace005
zalven-official <zalvendayao888@gmail.com>
2026-03-05 15:25:28 +0800

    succesfully removed the watermark

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,5 @@
 use lopdf::Document;
+use std::io::Cursor;
 
 pub fn prepend_blank_page_html(html: &str) -> String {
     let blank_page = r#"
@@ -12,15 +13,13 @@ pub fn prepend_blank_page_html(html: &str) -> String {
     format!("{}{}", blank_page, html)
 }
 
-pub fn remove_first_page(pdf_bytes: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut doc = Document::load_mem(&pdf_bytes)?;
-    let pages = doc.get_pages();
-    if pages.len() <= 1 {
-        return Ok(pdf_bytes);
-    }
-    let first_page = *pages.keys().next().unwrap();
-    doc.delete_pages(&[first_page]);
-    let mut output = Vec::new();
-    doc.save_to(&mut output)?;
-    Ok(output)
+pub fn remove_first_page(
+    input_bytes: Vec<u8>,
+) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
+    let mut doc = Document::load_from(Cursor::new(input_bytes))?;
+    doc.delete_pages(&[1]);
+    doc.prune_objects();
+    let mut out_buffer = Vec::with_capacity(doc.objects.len() * 128);
+    doc.save_to(&mut out_buffer)?;
+    Ok(out_buffer)
 }
